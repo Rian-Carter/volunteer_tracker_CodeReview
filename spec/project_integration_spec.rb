e738789f4bf837c0d08b144346afd0f9a615753a
Rian Carter <R.Carter86@hotmail.com>
2022-05-21 20:11:56 -0700

    WIP

--- spec/project_integration_spec.rb
+++ spec/project_integration_spec.rb
@@ -15,3 +15,28 @@ describe 'the project cration path', {:type => :feature} do
   end
 end
 
+# describe 'the project update path', {:type => :feature} do
+#   it 'allows a user to change the name of the project' do
+#     test_project = Project.new({:title => 'Teaching kids to code', :id => nil})
+#     test_project.save
+#     visit '/'
+#     click_link('Teaching kids to code')
+#     click_link('Edit Project')
+#     fill_in('title', :with => 'Teaching ruby to kids')
+#     click_button('Update')
+#     save_and_open_page
+#     expect(page).to have_content('Teaching ruby to kids')
+#   end
+# end
+
+# describe 'the project delete path', {:type => :feature} do
+#   it 'allows a user to delete a project' do
+#     test_project = Project.new({:title => 'Teaching kids to code', :id => nil})
+#     test_project.save
+#     visit "/projects/#{test_project.id}"
+#     click_link('Edit Project')
+#     click_button('Delete Volunteer')
+#     visit '/'
+#     expect(page).not_to have_content('Teaching kids to code')
+#   end
+# end
