ad435901eb020e769117ad7ca36e4ad58f13e917
Rian Carter <R.Carter86@hotmail.com>
2022-05-23 19:46:47 -0700

    clear out commented out code

--- app.rb
+++ app.rb
@@ -20,38 +20,28 @@ post('/projects') do
   title = params[:title]
   project = Project.new({:title => title, :id => nil})
   project.save
-  # @projects = Project.all
   redirect to('/projects')
-  # erb(:projects)
 end
 
 get('/projects/:id') do
   @project = Project.find(params[:id].to_i)
-  # erb(:edit_project)
   erb(:project)
 end
 
 get('/projects/:id/edit') do
-  @projects = Project.find(params[:id].to_i)
+  @project = Project.find(params[:id].to_i)
   erb(:edit_project)
 end
 
-# patch('/projects/:id/edit') do
-patch('/projects/:id') do
+patch('/projects/:id/edit') do
   @project = Project.find(params[:id].to_i)
-  # @project.update({:title => params[:title]})
-  @project.update(params[:title])
-  # @projects = Project.all
-  # erb(:projects)
+  @project.update({:title => params[:title]})
   redirect to('/projects')
 end
 
-# delete('/projects/:id/edit') do
 delete('/projects/:id') do
   @project = Project.find(params[:id].to_i)
   @project.delete
-  # @projects = Project.all
-  # erb(:projects)
   redirect to('/projects')
 end
 
@@ -63,22 +53,18 @@ post('/projects/:id/volunteers') do
 end
 
 get('/projects/:id/volunteers/:volunteer_id') do
-  # @volunteer = Volunteer.find(params[:project_id].to_i)
   @volunteer = Volunteer.find(params[:volunteer_id].to_i)
-  # erb(:project)
   erb(:volunteer)
 end
 
 patch('/projects/:id/volunteers/:volunteer_id') do
   @project = Project.find(params[:id].to_i)
-  # volunteer = Volunteer.find(params[:project_id].to_i)
   volunteer = Volunteer.find(params[:volunteer_id].to_i)
   volunteer.update(params[:name], @project.id)
   erb(:project)
 end
 
 delete('/projects/:id/volunteers/:volunteer_id') do
-  # volunteer = Volunteer.find(params[:project_id].to_i)
   volunteer = Volunteer.find(params[:volunteer_id].to_i)
   volunteer.delete
   @project = Project.find(params[:id].to_i)
